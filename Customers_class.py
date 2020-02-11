c314ca16bc701d1bb42717ff4733ee2fd365976f
Jason Robinson <jrobinson@spartaglobal.com>
2020-02-11 19:26:27 +0000

    added payment details method

--- Customers_class.py
+++ Customers_class.py
@@ -1,3 +1,25 @@
 from People_class import People
+
 class Customer(People):
-    pass
+    def __init__(self, name, address=None):
+        super().__init__(name)
+        if address is None:
+            address = []
+        else:
+            self.address = address
+        self.__payment_details = {}
+
+    def set_payment_details(self, address, card_no):
+        self.__payment_details['address'] = address
+        self.__payment_details['card number'] = card_no
+
+    def get_payment_details(self):
+        return self.__payment_details
+
+    
+
+
+
+#address details , payment details
+#alter ingredient
+#combo method?
