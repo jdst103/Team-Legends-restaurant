49ac1d8ac7bd91b08c500f264b086f1afbd43698
Avraj Singh <ASingh@spartaglobal.com>
2020-02-12 11:59:56 +0000

    added and updated people customer and staff

--- Customers_class.py
+++ Customers_class.py
@@ -1,13 +1,17 @@
 from People_class import People
 
+
 class Customer(People):
-    def __init__(self, name, address=None):
-        super().__init__(name)
+    def __init__(self, first_name, last_name, address=None):
+        super().__init__(first_name, last_name)
         if address is None:
             address = []
         else:
             self.address = address
         self.__payment_details = {}
+        self.first_name = first_name
+        self.last_name = last_name
+        self.staff_email = self.first_name + '.' + self.last_name + ".@restautant.com"
 
     def set_payment_details(self, address, card_no):
         self.__payment_details['address'] = address
@@ -16,10 +20,6 @@ class Customer(People):
     def get_payment_details(self):
         return self.__payment_details
 
-    
-
-
-
-#address details , payment details
-#alter ingredient
-#combo method?
+# address details , payment details
+# alter ingredient
+# combo method?
